714655ad3be116687475083372394a7e92311717
Siedrix <Siedrix@gmail.com>
2014-08-23 13:18:09 -0700

    Add basic example

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
-var express = require('express');
+var express = require('express'),
+	mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/samplebox');
+
+var Path = mongoose.model('Path', {
+	path: String,
+	views: {type: Number, default: 1}
+});
+
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+	Path.findOne({path:'/'}, function (err, path) {
+		if(err){
+			return res.status(500).send('Something went wrong');
+		}
+
+		if(path){
+			path.views++;
+
+			path.save(function (err) {
+				if(err){
+					return res.status(500).send('Something went wrong');
+				}
+				res.send('This page have been shown '+ path.views);
+			})
+		}else{
+			path = new Path({
+				path : '/',
+				views : 1
+			});
+
+			path.save(function (err) {
+				if(err){
+					return res.status(500).send('Something went wrong');
+				}
+				res.send('You are the first');
+			});
+		}
+	});
 });
 
 app.listen(3000);
